331ef796eb1d7bc41b8cf66fe417b79c16b5cf2b
matthewdalex <matthewalex@linux.com>
2017-07-06 21:37:10 -0400

    workqueue: change permissions to allow root control of wq_power_efficient toggle

--- drivers/mmc/core/core.c
+++ drivers/mmc/core/core.c
@@ -76,7 +76,7 @@ static const unsigned freqs[] = { 400000, 300000, 200000, 100000 };
  */
 
 bool use_spi_crc = 0;
-module_param(use_spi_crc, bool, 644);
+module_param(use_spi_crc, bool, 0644);
 
 static int mmc_schedule_delayed_work(struct delayed_work *work,
 				     unsigned long delay)
